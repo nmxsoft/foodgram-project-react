c021c076e11c3d5faa0905720916ebd54bda68d3
Max Nmx <nmxsoft@gmail.com>
2022-08-31 21:53:51 +0300

    add migrations

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,11 +1,9 @@
-from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.urls import reverse
 
 from .validators import validate_not_empty
-
-User = get_user_model()
+from users.models import User
 
 
 class Ingredient(models.Model):
